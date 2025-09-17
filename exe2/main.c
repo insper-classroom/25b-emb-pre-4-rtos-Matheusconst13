15a72b90a542ddc3c4471d102be5b5425a5b90cf
Matheusconst13 <matheuscsc1@al.insper.edu.br>
2025-09-17 18:32:24 -0300

    kadmsadjapsu

--- exe2/main.c
+++ exe2/main.c
@@ -116,6 +116,7 @@ int main() {
   xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
   xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
   vTaskStartScheduler();
+  
 
   while (true)
     ;
